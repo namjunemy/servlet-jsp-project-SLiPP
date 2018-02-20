df0fc75cbbd8f0b44776d0b96820a4bae425027b
Namjun Kim <namjunemy@gmail.com>
2018-02-21 02:30:47 +0900

    1. 개발환경 세팅 및 Servlet 기본

---
+++ src/io/namjune/HelloWorldServlet.java
@@ -0,0 +1,20 @@
+package io.namjune;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloWorldServlet extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    PrintWriter out = resp.getWriter();
+    String name = req.getParameter("name");
+    out.println("<h1>Hello World "+ name +"!!</h1>");
+  }
+}
