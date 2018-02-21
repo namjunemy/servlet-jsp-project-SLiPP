196f3a8424e5aab7b36622c7c37b681069bc8203
Namjun Kim <namjunemy@gmail.com>
2018-02-22 00:16:29 +0900

    modify embedded tomcat, Servlet&JSP exam

--- src/io/namjune/HelloWorldServlet.java
+++ src/io/namjune/HelloWorldServlet.java
@@ -1,7 +1,6 @@
 package io.namjune;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,12 +8,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/hello")
+@WebServlet(name = "HelloWorld", urlPatterns = { "/" })
 public class HelloWorldServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    PrintWriter out = resp.getWriter();
     String name = req.getParameter("name");
-    out.println("<h1>Hello World "+ name +"!!</h1>");
+    resp.getWriter().print(name + " Hello World!");
   }
 }
