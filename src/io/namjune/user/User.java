23fa264feed0a6280590315c002a115e7ca51fd3
Namjun Kim <namjunemy@gmail.com>
2018-03-07 00:52:13 +0900

    회원가입 화면, 개인정보 수정 화면의 중복 제거

--- src/io/namjune/user/User.java
+++ src/io/namjune/user/User.java
@@ -8,6 +8,9 @@ public class User {
   private String name;
   private String email;
 
+  public User() {
+  }
+
   public User(String userId, String password, String name, String email) {
     super();
     this.userId = userId;
