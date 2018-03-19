40a7994dbbbd48d60f23c80672c3ed40cef9b898
Namjun Kim <namjunemy@gmail.com>
2018-03-19 23:54:49 +0900

    DAO 리팩토링 - insert, update, delete 중복 제거. 공통 부분 클래스로 생성 후 추상 메소드를 이용한 처리 방법

--- src/io/namjune/user/UserDao.java
+++ src/io/namjune/user/UserDao.java
@@ -6,6 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import io.namjune.support.JdbcTemplate;
+
 public class UserDao {
   public Connection getConnection() {
     String url = "jdbc:mysql://localhost:3306/slipp";
@@ -22,25 +24,17 @@ public class UserDao {
   }
 
   public void addUser(User user) throws SQLException {
+    JdbcTemplate template = new JdbcTemplate() {
+      @Override
+      public void setParameters(PreparedStatement pstmt) throws SQLException {
+        pstmt.setString(1, user.getUserId());
+        pstmt.setString(2, user.getPassword());
+        pstmt.setString(3, user.getName());
+        pstmt.setString(4, user.getEmail());
+      }
+    };
     String sql = "insert into USERS values(?, ?, ?, ?)";
-
-    Connection conn = null;
-    PreparedStatement pstmt = null;
-    try {
-      conn = getConnection();
-      pstmt = conn.prepareStatement(sql);
-      pstmt.setString(1, user.getUserId());
-      pstmt.setString(2, user.getPassword());
-      pstmt.setString(3, user.getName());
-      pstmt.setString(4, user.getEmail());
-
-      pstmt.executeUpdate();
-    } finally {
-      if (pstmt != null)
-        pstmt.close();
-      if (conn != null)
-        conn.close();
-    }
+    template.executeUpdate(sql);
   }
 
   public User findByUserId(String userId) throws SQLException {
@@ -72,42 +66,27 @@ public class UserDao {
   }
 
   public void removeUser(String userId) throws SQLException {
+    JdbcTemplate template = new JdbcTemplate() {
+      @Override
+      public void setParameters(PreparedStatement pstmt) throws SQLException {
+        pstmt.setString(1, userId);
+      }
+    };
     String sql = "delete from USERS where userId = ?";
-
-    Connection conn = null;
-    PreparedStatement pstmt = null;
-    try {
-      conn = getConnection();
-      pstmt = conn.prepareStatement(sql);
-      pstmt.setString(1, userId);
-
-      pstmt.executeUpdate();
-    } finally {
-      if (pstmt != null)
-        pstmt.close();
-      if (conn != null)
-        conn.close();
-    }
+    template.executeUpdate(sql);
   }
 
   public void updateUser(User user) throws SQLException {
+    JdbcTemplate template = new JdbcTemplate() {
+      @Override
+      public void setParameters(PreparedStatement pstmt) throws SQLException {
+        pstmt.setString(1, user.getPassword());
+        pstmt.setString(2, user.getName());
+        pstmt.setString(3, user.getEmail());
+        pstmt.setString(4, user.getUserId());
+      }
+    };
     String sql = "update USERS set password = ?, name = ?, email = ? where userId = ?";
-
-    Connection conn = null;
-    PreparedStatement pstmt = null;
-    try {
-      conn = getConnection();
-      pstmt = conn.prepareStatement(sql);
-      pstmt.setString(1, user.getPassword());
-      pstmt.setString(2, user.getName());
-      pstmt.setString(3, user.getEmail());
-      pstmt.setString(4, user.getUserId());
-      pstmt.executeUpdate();
-    } finally {
-      if (pstmt != null)
-        pstmt.close();
-      if (conn != null)
-        conn.close();
-    }
+    template.executeUpdate(sql);
   }
 }
